738c4b316f1ea2fe8f17bef99197eb5d87e7e426
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2024-01-01 20:54:48 +0530

    introduced some commom alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,22 @@
 set fish_greeting
+#setting custom alias
+alias vim='nvim'
+alias apt='nala'
+
+#setting up custom functions
+
+# addcommit ()
+# git add . && git commit 
+function addcommit 
+  git add .
+  git commit
+end
+
+# gpush()
+# git push -u origin main 
+function gpush 
+  git push -u origin main
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
